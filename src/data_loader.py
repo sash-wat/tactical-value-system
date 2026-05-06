4a6157682dc6e3a9d74c9be576f509d57d18c5c1
Venkatesh, Sashwat Ravi <sashvenk@terpmail.umd.edu>
2026-05-06 18:33:17 -0400

    fix: retrieve goals added and unpack features

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,8 +3,19 @@ import pandas as pd
 
 def load_team_xgoals(leagues, season):
     asa = AmericanSoccerAnalysis()
-    # Fetch team xgoals which contains g+ components at team level
-    df = asa.get_team_xgoals(leagues=leagues, season_name=season)
+    # Fetch team goals added which contains g+ components at team level
+    df_gplus = asa.get_team_goals_added(leagues=leagues, season_name=season)
+    
+    rows = []
+    for idx, row in df_gplus.iterrows():
+        team_id = row['team_id']
+        team_data = {'team_id': team_id}
+        for item in row['data']:
+            action = item['action_type'].lower()
+            team_data[action] = item['goals_added_for']
+        rows.append(team_data)
+        
+    df = pd.DataFrame(rows)
     teams_df = asa.get_teams(leagues=leagues)
     df = df.merge(teams_df[['team_id', 'team_name']], on='team_id', how='left')
     return df
