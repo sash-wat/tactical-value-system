7b85580f22ff261dd4322fe4416f7efc98f7262a
Venkatesh, Sashwat Ravi <sashvenk@terpmail.umd.edu>
2026-05-06 20:42:16 -0400

    refactor: remove obsolete test and debug scripts

--- test_pass.py
+++
@@ -1,7 +0,0 @@
-from itscalledsoccer.client import AmericanSoccerAnalysis
-import pandas as pd
-asa = AmericanSoccerAnalysis()
-
-df_pass = asa.get_team_xpass(leagues='mls', season_name='2025')
-print("Passing columns:")
-print(df_pass.columns)
